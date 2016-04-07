3d29e5f2b05afa9c8b2cc803ca72d126842e1657
achlubek <luby.pl@gmail.com>
2016-04-07 21:36:40 +0200

    Commited missing files Thanks Asif

---
+++ VEngineNative/MaterialNode.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Texture.h";
+
+#define NODE_MODE_ADD 0
+#define NODE_MODE_MUL 1
+#define NODE_MODE_AVERAGE 2
+#define NODE_MODE_SUB 3
+#define NODE_MODE_ALPHAMIX 4
+#define NODE_MODE_REPLACE 5
+
+#define NODE_TARGET_DIFFUSE 0
+#define NODE_TARGET_NORMAL 1
+#define NODE_TARGET_ROUGHNESS 2
+#define NODE_TARGET_METALNESS 3
+#define NODE_TARGET_BUMP 4
+
+class MaterialNode
+{
+public:
+    MaterialNode(Texture *tex, glm::vec2 uvScaling, int mixMode, int nodeTarget);
+    MaterialNode();
+    ~MaterialNode();
+    Texture *texture;
+    glm::vec2 uvScale;
+    int mixingMode;
+    int target;
+};
+
